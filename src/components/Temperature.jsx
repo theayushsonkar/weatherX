fca1376f060526afded926432d5546f5e381df86
theayushsonkar <theayushsonkar@gmail.com>
2023-11-03 17:57:22 +0530

    UI Enhancements

--- src/components/Temperature.jsx
+++ src/components/Temperature.jsx
@@ -1,15 +1,26 @@
 import "./Temperature.css";
 
-const Temperature = ({apidata}) => {
-  return (
-    <div className="temperature">
-      {
-        (apidata?.current_weather?.temperature)? 
-        ( apidata?.current_weather?.temperature + " °C") 
-        : '🌏 weather'
-      }
-    </div>
-  );
+const Temperature = ({ apidata }) => {
+  let home = true;
+  if (apidata?.current?.temperature_2m) home = false;
+
+  if (home) {
+    return <div className="temperature">🌦️</div>;
+  } else
+    return (
+      <div className="temperature">
+        {/* <p className="tem">
+          {">"} Temperature
+        </p> */}
+        {apidata?.current?.temperature_2m + " °C"}
+        <p className="app-temp">
+          Feels like :
+          <span >
+          {" "+ apidata?.current?.apparent_temperature + " °C"}
+          </span>
+        </p>
+      </div>
+    );
 };
 
 export default Temperature;
